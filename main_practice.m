5198469b87dc1fc6df1a83a81ec84fbfa3a394f9
David Huberdeau <david.huberdeau@gmail.com>
2019-03-20 00:25:56 -0400

    Added instruction text to practice

--- main_practice.m
+++ main_practice.m
@@ -5,17 +5,25 @@ screenNumber=max(screens);
 screen_dims = [1600, 900];
 home_position = screen_dims/2;
 
+im_list = cell(1,5);
+for i_im = 1:length(im_list)
+    im_list{i_im} = imread(['instructions_pic_', num2str(i_im), '.png']);
+end
 
-keyIsDown = 0;
-text_drawn = 0;
-while ~keyIsDown
-    [ keyIsDown, keyTime, keyCode ] = KbCheck;  
-    if ~text_drawn
-        Screen('DrawText', win, 'test', home_position(1), home_position(2), [255 255 255]);
-        Screen('Flip', win)
-        text_drawn = 1;
+for i_im = 1:length(im_list)
+    keyIsDown = 0;
+    text_drawn = 0;
+    while ~keyIsDown
+        [ keyIsDown, keyTime, keyCode ] = KbCheck;  
+        if ~text_drawn
+        %         Screen('DrawText', win, 'test', home_position(1), home_position(2), [255 255 255]);
+            temp_tx = Screen('MakeTexture', win, im_list{i_im});
+            Screen('DrawTexture', win, temp_tx);
+            Screen('Flip', win)
+            text_drawn = 1;
+        end
+        pause(.017);
     end
-    pause(.017);
 end
 
 sca
